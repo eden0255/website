a9687adf39b78c3f8d2c29082431116e554df4f3
EdenHu <edenhu0255@gmail.com>
2024-04-19 21:16:44 +0200

    hello world

--- hallo.py
+++ hallo.py
@@ -2,6 +2,23 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return """<p>Hello, World!</p>
+<a href="/me" >
+  Click here
+</a>"""
+@app.route("/me")
+def eden():
+    return """<p>Hello, Eden!</p>
+<a href="/papa" >
+  Click here
+</a>"""
+
+@app.route("/papa")
+def papa():
+    return """<p>Hello, papa!</p>
+<a href="/mama" >
+  Click here
+</a>"""
